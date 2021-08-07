9f9c0ba35bf79d3e13f42b16c77449446afe15b4
Burak <burak.dursunlar@hotmail.com>
2021-08-07 19:23:19 +0300

    Do not initialize db using sql files

---
+++ user-service/src/main/java/com/github/buyalsky/userservice/dto/ShoppingCartDto.java
@@ -0,0 +1,13 @@
+package com.github.buyalsky.userservice.dto;
+
+public class ShoppingCartDto {
+    private String customerId;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+}
--- user-service/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-create table users (
-   id  serial not null,
-   address varchar(255),
-   birthday date,
-   email_address varchar(255),
-   full_name varchar(255),
-   gender varchar(255),
-   primary key (id)
-);
